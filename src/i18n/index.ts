4bb243bbc994dd078b87d07f96c556f6fadc59f9
Rasool <rasoolol.77@gmail.com>
2026-01-25 13:07:30 +0330

    feat: update i18n index file

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,17 @@ import { LANGUAGES } from '@/constants';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import enAbout from './locales/en/about.json';
 import enAuth from './locales/en/auth.json';
 import enCommon from './locales/en/common.json';
+import enDemo from './locales/en/demo.json';
+import enHome from './locales/en/home.json';
 import enPanel from './locales/en/panel.json';
+import faAbout from './locales/fa/about.json';
 import faAuth from './locales/fa/auth.json';
 import faCommon from './locales/fa/common.json';
+import faDemo from './locales/fa/demo.json';
+import faHome from './locales/fa/home.json';
 import faPanel from './locales/fa/panel.json';
 
 const detectionOptions = {
@@ -35,11 +41,25 @@ i18n
     detection: detectionOptions,
 
     resources: {
-      en: { common: enCommon, auth: enAuth, panel: enPanel },
-      fa: { common: faCommon, auth: faAuth, panel: faPanel },
+      en: {
+        common: enCommon,
+        auth: enAuth,
+        panel: enPanel,
+        home: enHome,
+        about: enAbout,
+        demo: enDemo,
+      },
+      fa: {
+        common: faCommon,
+        auth: faAuth,
+        panel: faPanel,
+        home: faHome,
+        about: faAbout,
+        demo: faDemo,
+      },
     },
 
-    ns: ['common', 'auth', 'panel'],
+    ns: ['common', 'auth', 'panel', 'home', 'about', 'demo'],
     defaultNS: 'common',
 
     interpolation: {
